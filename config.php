783c53aefccc88b17a67930cccefeaf5c68b20a8
JASONGODS <gtgusion@gmail.com>
2025-02-11 14:28:18 +0700

    menambahkan config

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+$database = "Inventory";
+
+$conn = mysqli_connect($server, $user, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Gagal terhubung dengan database :  " . $conn->connect_error);
+}
+?>
