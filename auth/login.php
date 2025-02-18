e189c71f1b197aadf997137628656b867505d1f7
richard-eng777 <moonelves.we@gmail.com>
2025-02-18 14:27:56 +0700

    Ganti design

--- auth/login.php
+++ auth/login.php
@@ -5,11 +5,11 @@
     <!-- Logo -->
     <div class="app-brand justify-content-center">
       <a href="index.html" class="app-brand-link gap-2">
-        <span class="app-brand-text demo text-uppercase fw-bolder">IdeKreatif</span>
+        <span class="app-brand-text demo text-uppercase fw-bolder">Inventory</span>
       </a>
     </div>
     <!-- /Logo -->
-    <h4 class="mb-2">Selamat datang di IdeKreatif! 👋</h4>
+    <h4 class="mb-2">Selamat datang di Inventory Management! 👋</h4>
     <form class="mb-3" action="login_auth.php" method="POST">
       <div class="mb-3">
         <label class="form-label">Username</label>
