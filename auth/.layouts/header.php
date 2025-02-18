402f48f6c56bd7a2702bcbd01cf6fd356f296b11
JASONGODS <gtgusion@gmail.com>
2025-02-18 14:04:10 +0700

    menambahkan layout header

--- auth/.layouts/header.php
+++ auth/.layouts/header.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+$notification = $_SESSION['notification'] ?? null;
+if ($notification) {
+  unset($_SESSION['notification']);
+}
+if (isset($_SESSION["username"]) || isset($_SESSION["role"])) {
+  $_SESSION['notification'] = [
+    'type' => 'danger',
+    'message' => 'Silahkan Logout Terlebih Dahulu !'
+  ];
+  header("Location: ../dashboard.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
