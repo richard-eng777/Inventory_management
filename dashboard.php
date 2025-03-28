44774d9f09e9f9f1e0668db02cebbbd9c2926d25
JASONGODS <gtgusion@gmail.com>
2025-03-28 17:27:02 +0700

    menambahkan table di dashboard

--- dashboard.php
+++ dashboard.php
@@ -3,94 +3,161 @@ include (".includes/header.php");
 $title = "Dashboard";
 include '.includes/toast_notification.php';
 ?>
-<div class="container-xxl flex-grow-1 container-p-y">
-    <div class="card">
-        <div class="card">
-            <div class="card-header d-flex justify-content-between align-items-center">
-                <h4>Semua produk</h4>
+<!-- Bootstrap Dark Table -->
+<div class="card">
+  <h5 class="card-header">Table Dark</h5>
+  <div class="table-responsive text-nowrap">
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th>Project</th>
+          <th>Client</th>
+          <th>Users</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody class="table-border-bottom-0">
+        <tr>
+          <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Angular Project</strong></td>
+          <td>Albert Cook</td>
+          <td>
+            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Lilian Fuller"
+              >
+                <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" />
+              </li>
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Sophia Wilkerson"
+              >
+                <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle" />
+              </li>
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Christina Parker"
+              >
+                <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle" />
+              </li>
+            </ul>
+          </td>
+          <td><span class="badge bg-label-primary me-1">Active</span></td>
+          <td>
+            <div class="dropdown">
+              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                <i class="bx bx-dots-vertical-rounded"></i>
+              </button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="javascript:void(0);"
+                  ><i class="bx bx-edit-alt me-1"></i> Edit</a
+                >
+                <a class="dropdown-item" href="javascript:void(0);"
+                  ><i class="bx bx-trash me-1"></i> Delete</a
+                >
+              </div>
             </div>
-            <div class="card-body">
-                <div class="table-responsive text-nowrap">
-                    <table id="datatable" class="table table-hover">
-                        <thead>
-                            <tr class="text-center">
-                                <th width="50px">#</th>
-                                <th>Judul Post</th>
-                                <th>Penulis</th>
-                                <th>Kategori</th>
-                                <th width="150px">Pilihan</th>
-                            </tr>
-                        </thead>
-                        <tbody class="table-border-bottom-0">
-
-                        <?php
-                        $index = 1;
-                        $query = "SELECT pengiriman.*, barang.nama as user_nama,
-                        penyedia.nama as user_name, FROM pengiriman
-                        INNER JOIN barang ON pengiriman.barang_id = barang.barang_id
-                        LEFT JOIN penyedia ON pengiriman.penyedia_id = penyedia.penyedia_id
-                        WHERE pengiriman.user_name = $userId";
-                        
-
-                        $exec = mysqli_query($conn, $query);
-
-                        while ($post = mysqli_fetch_assoc($exec)) :
-                        ?>
-
-                        <tr>
-                            <td><?= $index++; ?></td>
-                            <td><?= $barang['user_name']; ?></td>
-                            <td><?= $barang['user_nama']; ?></td>
-                            <td><?= $barang['tanggal_pengiriman']; ?></td>
-                            <td>
-                                <div class="dropdown">
-                                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                                        <i class="bx bx-dots-vertical-rounded"></i>
-                                    </button>
-
-                                    <div class="dropdown-menu">
-                                        <a href="edit_barang.php?id_barang=<?= $barang['barang_id']; ?>"class="dropdown-item">
-                                            <i class="bx bx-edit-alt me-2"></i> Edit
-                                        </a>
-
-                                        <a href="#" class="dropdown-item" data-bs-toggle="modal"
-                                        data-bs-target="#deletePost_<?= $post['barang_id']; ?>">
-                                            <i class="bx bx-trash me-2"></i> Delete 
-                                        </a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <div class="modal fade" id="deletePost_<?= $post['id_post']; ?>"tabindex="-1" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title">Hapus Post?</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <form action="proses_post.php" method="POST">
-                                            <div>
-                                                <p>Tindakan ini tidak bisa dibatalkan.</p>
-                                                <input type="hidden" name="postID" value="<?= $post['id_post']; ?>">
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
-                                                <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <?php endwhile; ?>
-                        </tbody>
-                    </table>
-                </div>
+          </td>
+        </tr>
+        <tr>
+          <td><i class="fab fa-react fa-lg text-info me-3"></i> <strong>React Project</strong></td>
+          <td>Barry Hunter</td>
+          <td>
+            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Lilian Fuller"
+              >
+                <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" />
+              </li>
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Sophia Wilkerson"
+              >
+                <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle" />
+              </li>
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Christina Parker"
+              >
+                <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle" />
+              </li>
+            </ul>
+          </td>
+          <td><span class="badge bg-label-success me-1">Completed</span></td>
+          <td>
+            <div class="dropdown">
+              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                <i class="bx bx-dots-vertical-rounded"></i>
+              </button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="javascript:void(0);"
+                  ><i class="bx bx-edit-alt me-1"></i> Edit</a
+                >
+                <a class="dropdown-item" href="javascript:void(0);"
+                  ><i class="bx bx-trash me-1"></i> Delete</a
+                >
+              </div>
             </div>
-        </div>
-    </div>
-</div>
+          </td>
+        </tr>
+        <tr>
+          <td><i class="fab fa-vuejs fa-lg text-success me-3"></i> <strong>VueJs Project</strong></td>
+          <td>Trevor Baker</td>
+          <td>
+            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Lilian Fuller"
+              >
+                <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" />
+              </li>
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Sophia Wilkerson"
+              >
+                <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle" />
+              </li>
+              <li
+                data-bs-toggle="tooltip"
+                data-popup="tooltip-custom"
+                data-bs-placement="top"
+                class="avatar avatar-xs pull-up"
+                title="Christina Parker"
+              >
+                <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle" />
+              </li>
+            </ul>
+          </td>
+          <td><span class="badge bg-label-info me-1">Scheduled</span></td>
+          <td>
+            <div class="dropdown">
+              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data
 <?php
 include (".includes/footer.php");
 ?>
