c908df5e6aea7d21e7c2333053f9c55d823ebb6c
JASONGODS <gtgusion@gmail.com>
2025-04-05 13:49:54 +0700

    anjengggggggg

--- proses_barang.php
+++ proses_barang.php
@@ -7,9 +7,10 @@ $userId = $_SESSION["user_id"];
 
 if (isset($_POST['simpan'])) {
     // Mengambil data dari form
-    $nama = $_POST['nama'];
-    $jumlah = $_POST['jumlah'];
-    $harga = $_POST['harga'];
+    $namabarang = $_POST['name'];
+    $penyediaid = $_POST['penyedia_id'];
+    $jumlahbarang = $_POST['jumlah'];
+    $hargabarang = $_POST['harga'];
 
     $query = "INSERT INTO barang (`name`, penyedia_id, jumlah, harga) values ('$namabarang', '$penyediaid', '$jumlahbarang', '$hargabarang')";
    
